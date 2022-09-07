1d8a6ddb7f597c209d13b015dc17a86e99d7490e
Emil Dalalyan <emil.dalalyan@gmail.com>
2022-09-07 16:02:49 +0300

    Updated PI constant

--- math.h
+++ math.h
@@ -3,7 +3,7 @@
 #include <math.h>
 #include "build.h"
 
-#define MATH_PI 3.141592
+#define MATH_PI 3.1415926535897932
 #define MATH_E  2.71828
 
 /* Calculate the "base" logarithm of number "num". */
